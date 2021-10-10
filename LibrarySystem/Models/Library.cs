e3d70d16035ef85f44221d566c6f851f7934cbdb
BK014 <simmikaur297@gmail.com>
2021-10-10 22:37:34 +1300

    Add project files.

---
+++ LibrarySystem/Models/Library.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Models
+{
+    public class Library
+    {
+        //the attributes that need to be filled
+        public int Id { get; set; } // id for library 
+        public string Name { get; set; } //  Name for person
+        public int LibraryPopulation { get; set; } // add population in the library 
+        public string LibraryAddress { get; set; } // Address  for the person 
+    }
+}
